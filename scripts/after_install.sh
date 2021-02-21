6c48f3f0f14781cc765fe17a1402b53276cf7d0b
John Long <kiino@qq.com>
2021-02-20 20:14:28 -0700

    remove docker and codedeploy

--- scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#navigate into our working directory where we have all our github files
-cd /home/ubuntu/express-app
-
-#install node modules
-npm install
--- scripts/application_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#start our node app in the background
-sudo npm run serve
